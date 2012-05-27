b7e4a18448999509abf980aa3e3d738d6821c149
Martin Atkins <mart@degeneration.co.uk>
2012-05-27 08:02:13 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="templatetk",
+    version="0.1",
+    description="Template compilation",
+    author="Armin Ronacher",
+    author_email="armin.ronacher@active-4.com",
+    packages=['templatetk'],
+)
